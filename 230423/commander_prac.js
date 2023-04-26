25cd7de8a227c565ccd2246aa07b80ea79656e76
HaekyungSeong <haekyungs95@gmail.com>
2023-04-26 22:44:14 +0900

    cli -v 가 파일로만 이동됨

--- 230423/commander_prac.js
+++ 230423/commander_prac.js
@@ -1,11 +1,12 @@
 import { program } from 'commander'
 
 program
-  .version('v1.0.0')
-  .name('Zoes_program')
+  .version('1.0.0','-v, --version')
+  .name('cli')
   .description('이 프로그램은 zoe가 만들고 zoe가 고생한거니까 감사히 여겨 불만은 받지 않는다.')
   .option('-n, --name <name>', 'your name')
 
-program.parse();
+program.parse(process.argv);
 
-// console.log(program)
+// console.log(pronpm gram)
+// npx cli -v 하면 되는 거 처럼 하곤 파일로 이동만 됨.
